cee0428ebe0ecd82510041aabdff6f1f8f7c5f4c
hieutran95 <hieutran1412@yahoo.com>
2018-02-09 21:19:19 -0800

    proj test

---
+++ p2p project/app/src/main/java/no/bouvet/p2pcommunication/adapter/DiscoveryListAdapterViewHolder.java
@@ -0,0 +1,19 @@
+package no.bouvet.p2pcommunication.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
+import no.bouvet.p2pcommunication.R;
+
+public class DiscoveryListAdapterViewHolder {
+
+    @BindView(R.id.discovered_device_name_text_view) TextView deviceNameTextView;
+    @BindView(R.id.discovered_device_status_text_view) TextView deviceStatusTextView;
+
+    public DiscoveryListAdapterViewHolder(View view) {
+        ButterKnife.bind(this, view);
+    }
+
+}
